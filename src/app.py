35e09d7b0fe2715ce85a571f1a6cac2b76131684
MVCSystems <carlosmerav@gmail.com>
2024-08-28 23:04:04 -0500

    Add

--- src/app.py
+++ src/app.py
@@ -1,32 +1,35 @@
 from flask import Flask,jsonify,request
+
 app = Flask(__name__)
 
-todos=[ { "label": "My first task", "done": False } ]
+todos=[ { "label": "My first task", "done": True } ]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    json_text = jsonify(todos)
-    return json_text 
-
+    return jsonify(todos)
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
-    request_body=request.get_json(force=True) 
-    todos.append(request_body)
-    json_text=jsonify(todos)
+    request_body = request.json
     print("Incoming request with the following body", request_body)
-    return json_text
-
+    todos.append(request_body)
+    return jsonify(todos)
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
-
-    print("This is the position to delete:", position)
+    print("This is the position to delete:", position)    
     todos.pop(position)
     return jsonify(todos)
 
-#pop sin arg elimina el ultimo,con argumento (position),elimina la posicion segun el indice
-
 # These two lines should always be at the end of your app.py file
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3245, debug=True)
+    try:
+        app.run(host='0.0.0.0', port=3245, debug=True)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Mantener el entorno virtual activo
+        while True:
+            pass
+
+
+
